7bebcfb2900875ca9a06941ca05ecd1c5cfa0bbc
Igor Novais <igorhnovaisdriven@gmail.com>
2023-03-17 19:15:16 -0300

    feat: Route /products

--- src/App/app.js
+++ src/App/app.js
@@ -4,7 +4,8 @@ import { Navigate } from "react-router-dom";
 import GlobalStyle from "../Components/globalStyle";
 import AuthProvider from "../Components/auth";
 import SignInPage from "../Sign/signInPage";
-import SignUpPage from "../Sign/signUpPage"
+import SignUpPage from "../Sign/signUpPage";
+import HomePage from "../Home/homePage"
 
 export default function App(){
     return (
@@ -13,9 +14,10 @@ export default function App(){
                 <GlobalStyle/>
                 <AuthProvider>
                     <Routes>
-                        <Route path={"/"} element={<Navigate replace to="/sign-in"/>}/>
+                        <Route path={"/"} element={<Navigate replace to="/products"/>}/>
                         <Route path={"/sign-in"} element={<SignInPage/>}/>
                         <Route path={"/sign-up"} element={<SignUpPage/>}/>
+                        <Route path={"/products"} element={<HomePage/>}/>
                     </Routes>
                 </AuthProvider>
 
