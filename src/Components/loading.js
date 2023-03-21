488ea0cf778ece599c648009b4044acbe6fbe1ef
Igor Novais <igorhnovaisdriven@gmail.com>
2023-03-21 16:09:57 -0300

    feat: loading

---
+++ src/Components/loading.js
@@ -0,0 +1,19 @@
+import { MagnifyingGlass } from "react-loader-spinner";
+import styled from "styled-components";
+
+export default function Loading(){
+    return (
+        <>
+            <Div>
+              <MagnifyingGlass width={"400"} height={"400"} color={"#322938"}/>
+            </Div>
+            
+        </>
+    )
+}
+
+const Div = styled.div`
+    justify-content: center;
+    align-items: center;
+
+`
