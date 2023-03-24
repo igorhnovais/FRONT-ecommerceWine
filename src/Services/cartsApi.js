208e7dd26a20252fb2950682dfbac7f0eddfd5f9
Igor Novais <igorhnovaisdriven@gmail.com>
2023-03-23 22:20:49 -0300

    feat: function findManyProductscart

--- src/Services/cartsApi.js
+++ src/Services/cartsApi.js
@@ -3,9 +3,18 @@ import api from "./api";
 export async function AddProductCart(body, token){
     
     const response = await api.post("/cart", body, {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        })
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    })
     return response.data;
+}
+
+export async function findManyProductsCart(token){
+    const response = await api.get("/cart", {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    })
+    return response;
 }
