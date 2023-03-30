40520050c75d1abea111e85e56c87b94bd8f41f7
Igor Novais <igorhnovaisdriven@gmail.com>
2023-03-30 16:05:34 -0300

    feat: getWines

--- src/Services/productsApi.js
+++ src/Services/productsApi.js
@@ -2,11 +2,16 @@ import api from "./api";
 
 export async function getAllProducts(){
     const response = await api.get("/products");
-    return response;
+    return response.data;
 }
 
 export async function getOneProduct(productId){
 
     const response = await api.get(`/product/${productId}`);
     return response.data;
+}
+
+export async function getWines(){
+    const response = await api.get("/products/wine");
+    return response.data
 }
