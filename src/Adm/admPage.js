901bab6775b01cff97a5b71c2f18253fc294588a
Igor Novais <igorhnovaisdriven@gmail.com>
2023-04-05 23:14:26 -0300

    feat

--- src/Adm/admPage.js
+++ src/Adm/admPage.js
@@ -1,12 +1,24 @@
 import styled from "styled-components";
+import axios from "axios";
+import Swal from 'sweetalert2';
 
 import useToken from "../Hooks/useToken";
 import useGetAdm from "../Hooks/Api/useGetAdm";
 import useProducts from "../Hooks/Api/useProducts";
 import AdmProductsList from "./admProductsList";
 import Loading from "../Components/loading"
+import { useState } from "react";
+import { useEffect } from "react";
 
 export default function AdmPage(){
+    const [name, setName] = useState("");
+    const [image, setImage] = useState("");
+    const [description, setDescription] = useState("");
+    const [type, setType] = useState("");
+    const [alcohol, setAlcohol] = useState("");
+    const [value, setValue] = useState("");
+    const [type_product, setType_product] = useState("");
+    const [reload, setReload] = useState([]);
 
     const token = useToken();
     const {adm} = useGetAdm(token);
@@ -20,21 +32,69 @@ export default function AdmPage(){
         </>)
     }
 
+    function addProduct(event){
+
+        event.preventDefault();
+
+        const registration = {
+            name,
+            image,
+            description,
+            type,
+            alcohol,
+            value, 
+            type_product
+        };
+
+        const promise = axios.post(`${process.env.REACT_APP_API_BASE_URL}/adm`, registration);
+
+        promise.then((resp => { 
+            getProducts();
+            setName('');
+            setImage('');
+            setDescription('');
+            setType('');
+            setAlcohol('');
+            setValue('');
+            setType_product('');
+            Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: 'Produto Adicionado',
+            showConfirmButton: false,
+            timer: 1500
+            })
+         }));
+
+        promise.catch((err) => {
+            Swal.fire({
+                position: 'center',
+                icon: 'error',
+                title: 'Não foi possivel adicionar',
+                showConfirmButton: false,
+                timer: 1500
+              })
+        });
+    }
+
     return (
         <>
             <H1>Seu painel de controle</H1>
             <Section>
-                <AddDiv>
-                    <h2> adicione aqui seu produto</h2>
-                    <input></input>
-                    <input></input>
-                    <input></input>
-                    <input></input>
-                    <input></input>
-                    <input></input>
-                    <input></input>
-                    <button>Adicionar</button>
+                <AddDiv onSubmit={addProduct}>
+                    <div>
+                        <h2> adicione aqui seu produto</h2>
+                        <input placeholder="Nome do produto" type="text" value={name} onChange={(e) => setName(e.target.value)} required></input>
+                        <input placeholder="Imagem" type="text" value={image} onChange={(e) => setImage(e.target.value)} required></input>
+                        <input placeholder="Descrição" type="text" value={description} onChange={(e) => setDescription(e.target.value)} required></input>
+                        <input placeholder="Tipo" type="text" value={type} onChange={(e) => setType(e.target.value)}></input>
+                        <input placeholder="Quantidade de alcool" type="text" value={alcohol} onChange={(e) => setAlcohol(e.target.value)}></input>
+                        <input placeholder="Valor" type="number" value={value} onChange={(e) => setValue(e.target.value)} required></input>
+                        <input placeholder="Tipo do Produto" type="text" value={type_product} onChange={(e) => setType_product(e.target.value)} required></input>
+                        <button type="submit">Adicionar</button>
+                    </div>
                 </AddDiv>
+                
                 <RemoveDiv>
                     <h2> Remova algum produto</h2>
                     <div> 
@@ -65,25 +125,57 @@ const Section = styled.section`
     display: flex;
     justify-content: center;
     margin-top: 20px;
+    border: 5px solid white;
+    background-color: rgb(159,140,212);
 `
 
-const AddDiv = styled.div`
-    background-color: red;
+const AddDiv = styled.form`
     width: 50%;
     display: flex;
     flex-direction: column;
     align-items: center;
+    border-right: 5px solid white;
     & h2{
         color: white;
         font-size: 35px;
+        margin: 10px 10px;
+        text-shadow: 2px 2px green;
     }
     & input{
-        width: 80%;
+        width: 100%;
+        margin-bottom: 5px;
+        height: 40px;
+    }
+    & div{
+        display: flex;
+        flex-direction: column;
+    }
+    & button{
+        background-color: green;
+        color: white;
+        font-size: 20px;
+        border: none;
+        border-radius: 3px;
+        margin-top: 10px;
+        height: 45px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.35);
+        border-radius: 5px;
+        cursor: pointer;
+        text-decoration: none;
+        user-select: none;
+        -webkit-user-select: none;
+        touch-action: manipulation;
+        :active {
+        box-shadow: #422800 2px 2px 0 0;
+        transform: translate(2px, 2px);
+        }
     }
 `
 
 const RemoveDiv = styled.div`
-    background-color: green;
     width: 50%;
     display: flex;
     flex-direction: column;
@@ -91,6 +183,8 @@ const RemoveDiv = styled.div`
     & h2{
         color: white;
         font-size: 35px;
+        margin: 10px 10px;
+        text-shadow: 2px 2px red;
     }
 `
 
--- src/Components/adm.js
+++ src/Components/adm.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import {RiAdminFill} from "react-icons/ri";
 
 import useToken from "../Hooks/useToken";
 import useGetAdm from "../Hooks/Api/useGetAdm";
@@ -12,9 +13,14 @@ export default function Adm(){
         <>
             {(adm === true)
                 ?
-                <Link to="/adm">
-                     <H6> Adm</H6>
-                </Link>
+                
+                <Div>
+                    <Link to="/adm">
+                        <RiAdminFill/>
+                        <h6>Adm</h6>
+                    </Link>
+                </Div>
+                
                 :
                 ""
             } 
@@ -22,6 +28,28 @@ export default function Adm(){
     )
 }
 
-const H6 = styled.h6`
-    color: white
+const Div = styled.div`
+    width: 200px;
+    height: 30px;
+    margin-top: 10px;
+    padding: 5px;
+    display: flex;
+    cursor:pointer;
+    transition: transform 0.3s ease-in-out;
+    &:hover {
+        transform: scale(1.1);
+    }
+    & h6{
+        color: white;
+        text-shadow: 0 0 6px white;
+        border-bottom: 2px solid #322938;
+    }
+    & svg{
+        color: red;
+        margin-right: 5px;
+    }
+    & a{
+        display: flex;
+        text-decoration: none;
+    }
 `
